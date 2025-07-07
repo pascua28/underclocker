6c19517f707b505502159d08c89ea314b6d47ce2
Samuel Pascua <sgpascua@ngcp.ph>
2025-07-07 16:44:13 +0800

    Enable minify
    Signed-off-by: Samuel Pascua <sgpascua@ngcp.ph>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,13 +12,11 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
